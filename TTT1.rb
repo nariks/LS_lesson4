255d62dbadca04be91c9fcffa1dabffb0b3c39a9
nariks <happycow@gmail.com>
2016-08-22 22:49:37 +1000

    features almost completed

--- TTT1.rb
+++ TTT1.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def prompt(message)
   puts "==> #{message}"
 end
@@ -9,6 +10,7 @@ def initialize_board
 end
 
 def display_board(board)
+  system "clear"
   (1..3).each do |row_count|
     (1..4).each do |count|
       if count == 3 
@@ -40,7 +42,7 @@ end
 
 def obtain_valid_input(board)
   prompt("Please enter a valid unmarked position number from 1-9")
-  loop do 
+  loop do
     input = gets.chomp
     if valid_input?(input)
       if position_unmarked?(board, input.to_f)
@@ -54,16 +56,60 @@ def obtain_valid_input(board)
   end
 end
 
-def player_marks_board(board, position)    
+def player_marks_board(board, position)
   row, col = convert_1d_to_2d(position)
   board[row][col - 1] = 'X'
   board
 end
 
+def computer_marks_board(board)
+  board.each do |k,v|
+    v.each_with_index do |value, index|
+      if value == ' '
+        board[k][index] = "O"
+        return board
+      end
+    end
+  end
+end
+
+def board_not_empty?(board)
+  board.values.flatten.include?(" ")
+end
+
+def transpose(board)
+
+end
+
+def diagonal(board)
+
+end
+
+def no_winner?(board)
+  board.values.each do |line|
+    return true if line[0] == line [1] && line[1] == line[2]
+  end
+  transpose(board.values).each do |line|
+    return true if line[0] == line [1] && line[1] == line[2]
+  end
+  diagonal(board.values).each do |line|
+    return true if line[0] == line[1] && line[1] == line[3]
+  end
+end
+
+
 board = initialize_board
 display_board(board)
 
 prompt("Enter board position to mark")
-player_input = obtain_valid_input(board)
-board = player_marks_board(board, player_input)
-display_board(board)
+loop do
+  if board_not_empty?(board) && no_winner?(board)
+    player_input = obtain_valid_input(board)
+    board = player_marks_board(board, player_input)
+    display_board(board)
+    board = computer_marks_board(board)
+    display_board(board)
+  else
+    puts detect_winner
+  end
+end
